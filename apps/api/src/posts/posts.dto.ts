a89d34c23350a322de73fd7121b1ff1a9ffcf909
Nikita Sklyar <nikskl999@gmail.com>
2026-05-25 13:54:54 +0300

    feat: add post page query

--- apps/api/src/posts/posts.dto.ts
+++ apps/api/src/posts/posts.dto.ts
@@ -36,6 +36,16 @@ export class UpdateCommentDto {
   body!: string;
 }
 
+export class GetPostsPageQueryDto {
+  static readonly schema = z.object({
+    limit: z.coerce.number().int().min(1).max(50).default(10),
+    offset: z.coerce.number().int().min(0).default(0),
+  });
+
+  limit!: number;
+  offset!: number;
+}
+
 export class UpdateReviewDto {
   static readonly schema = z.object({
     body: reviewBodySchema,
