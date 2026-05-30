7b70481585fbc62f24e4baa8fc73cd779e914e60
Nikita Sklyar <nikskl999@gmail.com>
2026-05-30 23:20:29 +0300

    refactor: read dev env from infra/.env.dev

--- apps/api/src/config/register-database-url.ts
+++ apps/api/src/config/register-database-url.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 import { getDatabaseUrl } from './database-url';
 
-config({ path: '../../.env' });
+config({ path: '../../infra/.env.dev' });
 config();
 
 process.env.DATABASE_URL = getDatabaseUrl();
